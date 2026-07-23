88568377cd2ec2334e4961c89e461489f87ab597
Aaahtemis <churaa1@student.op.ac.nz>
2026-07-24 10:46:04 +1200

    add piezo testing

---
+++ AppDev/pythonConcepts.py
@@ -0,0 +1,4 @@
+def studio_summary(name, class_count):
+    return f"{name} has {class_count} classes"
+
+studio_summary("Cityfit", 6)
--- Embedded/piezoTesting/piezoTesting.ino
+++ Embedded/piezoTesting/piezoTesting.ino
@@ -4,15 +4,19 @@ int input = 0;
 int lightStart = 2;
 int lightEnd = 8;
 
-int movingAverage[5] = { 0, 0, 0, 0, 0 };
-int simpleAverage[100];
+bool emaSeeded = false;
+float emAverage = 0;
+int sAverage[100];
 int lowerDeadZone = 100;
+int lowerMargin = 30;
 
-float smoothing = 0;
+float averageSmoothing = 0;
 
 int tickCount = 0;
-int tickCarryMoving = 5000;
-int tickCarrySimpleMod = 5;
+int tickCarryMoving = 1000;
+int tickCarrySimpleMod = 4;
+
+void AddToStartOfArray(int value, int array[], int length, bool doPrint = false);
 
 void setup() {
   Serial.begin(9600);
@@ -21,9 +25,12 @@ void setup() {
   for (int i = lightStart; i <= lightEnd; i++) {
     pinMode(i, OUTPUT);
   }
+  for (int i = 0; i < sizeof(sAverage) / sizeof(sAverage[0]); i++){
+    sAverage[i] = lowerMargin;
+  }
 
-  smoothing = 2/((sizeof(movingAverage) / sizeof(movingAverage[0]))+1);
-  Serial.println("smoothingMult = " + String(smoothing));
+  averageSmoothing = (float)2 / ((sizeof(sAverage) / sizeof(sAverage[0])) + 1);
+  Serial.println("smoothingMult = " + String(averageSmoothing));
 }
 
 void loop() {
@@ -31,9 +38,9 @@ void loop() {
 
   int rawData = analogRead(input);
   //Serial.println("raw data: " + String(rawData));
-  int safeValue = constrain(rawData, lowerDeadZone, 1023);
+  int safeValue = constrain(rawData, 0, 1023);
   //Serial.println("safe data: " + String(safeValue));
-  float normalizedValue = map(safeValue, lowerDeadZone, 1023, 0, 100);
+  float normalizedValue = map(safeValue, 0, 1023, 0, 100);
   //Serial.println("normalized data: " + String(normalizedValue));
 
   float currentLightStage = map(normalizedValue, 0, 100, lightStart, lightEnd);
@@ -45,27 +52,46 @@ void loop() {
     }
   }
 
-  if (tickCount % tickCarryMoving / tickCarrySimpleMod ){
-    if (safeValue != 0) AddToStartOfArray(safeValue, simpleAverage);
+  if (tickCount % tickCarryMoving / tickCarrySimpleMod) {
+    if (normalizedValue > lowerMargin){
+      AddToStartOfArray(normalizedValue, sAverage, sizeof(sAverage) / sizeof(sAverage[0]));
+      Serial.println(sAverage[0]);
+    }
+      FindExponentialAverage(sAverage[0]);
   }
 
-  if (tickCount >= tickCarryMoving) { // inner loop #1
-    AddToStartOfArray(safeValue, movingAverage);
+  if (tickCount >= tickCarryMoving) {  // inner loop #1
     tickCount = 0;
   }
 }
 
-void AddToStartOfArray(int value, int array[]) {
-  for (int i = 0; i < (sizeof(array) / sizeof(array[0])) - 1; i++) {
+void AddToStartOfArray(int value, int array[], int length, bool doPrint) {
+  for (int i = 0; i < length - 1; i++) {
     array[i + 1] = array[i];
   }
+
   array[0] = value;
 
-  for (int i = 0; i < (sizeof(array) / sizeof(array[0])); i++) {
-    Serial.println(array[i]);
+  if (doPrint) {
+    for (int i = 0; i < length; i++) {
+      Serial.println(array[i]);
+    }
   }
 }
 
-void FindExponentialAverage(){
-  
+float FindSimpleAverage(int array[], int length) {
+  long sum = 0;
+  for (int i = 0; i < length; i++) {
+    sum += array[i];
+  }
+  return (float)sum / length;
+}
+
+float FindExponentialAverage(int value) {
+  if (!emaSeeded) {
+    emAverage = FindSimpleAverage(sAverage, sizeof(sAverage) / sizeof(sAverage[0]));
+    emaSeeded = true;
+  }
+  emAverage = (value * averageSmoothing) + (emAverage * (1.0 - averageSmoothing));
+  return emAverage;
 }
